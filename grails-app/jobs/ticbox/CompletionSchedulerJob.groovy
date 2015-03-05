f87f81788a8d5c035737d41d980a3e331356db3f
Firman Agustian <firman@ticbox.co>
2015-03-05 12:59:52 +0700

    bug fix 393725

---
+++ grails-app/jobs/ticbox/CompletionSchedulerJob.groovy
@@ -0,0 +1,32 @@
+package ticbox
+
+import java.text.SimpleDateFormat
+
+
+class CompletionSchedulerJob {
+
+    static triggers = {
+      //simple repeatInterval: 5000l // execute job once in 5 seconds
+      cron name: 'MyTrigger', cronExpression: "0, 0, 0, *, *, ?"
+    }
+
+    def List<Survey> getAllInProgressSurvey() {
+        return Survey.findAllByStatus(Survey.STATUS.IN_PROGRESS)
+    }
+
+    def execute() {
+        // execute job
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy")
+        Date today = new Date()
+
+        for (Survey survey : getAllInProgressSurvey()) {
+            Date endDate =  formatter.parse(survey.getCompletionDateTo())
+
+            if(today.after(endDate)) {
+
+                survey.status = Survey.STATUS.COMPLETED
+                survey.save(flush: true)
+            }
+        }
+    }
+}
