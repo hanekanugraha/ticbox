93959f0c038bb87e831544d03d1dbeb34ef4dc9f
Firman Agustian <firman@ticbox.co>
2015-12-08 17:38:28 +0700

    fixing bug 393708

--- grails-app/controllers/ticbox/SchedulerController.groovy
+++ grails-app/controllers/ticbox/SchedulerController.groovy
@@ -19,28 +19,16 @@ class SchedulerController {
         Date date = new Date()
 
         try {
-            List<ServerAddress> seeds = new ArrayList<ServerAddress>();
-            seeds.add(new ServerAddress("kahana.mongohq.com", 10040))
-            // local
-//            seeds.add(new ServerAddress("localhost", 27017))
+            def listSurvey = Survey.findAllByCompletionDateTo(formatter.format(date.minus(1)))
 
+            if(listSurvey.size() != 0) {
 
-            List<MongoCredential> credentials = new ArrayList<MongoCredential>()
-            credentials.add(MongoCredential.createMongoCRCredential("ticboxnew", "ticboxnew","ticboxnew".toCharArray()));
-
-            MongoClient mongoClient = new MongoClient( seeds, credentials )
-            // local
-            // new MongoClient(seeds)
-
-
-            def mongo = new GMongo(mongoClient)
-            def db = mongo.getDB("ticboxnew")
-
-
-
-            db.survey.update([status:Survey.STATUS.IN_PROGRESS, completionDateTo:formatter.format(date.minus(1))],
-                             [$set:[status:Survey.STATUS.COMPLETED, enableStatus:Survey.ENABLE_STATUS.DISABLE]])
-
+                for(survey in listSurvey) {
+                    survey.setStatus(Survey.STATUS.COMPLETED);
+                    survey.setEnableStatus(Survey.ENABLE_STATUS.DISABLE)
+                }
+                Survey.saveAll(listSurvey);
+            }
 
             render "OK"
         }catch (e){
