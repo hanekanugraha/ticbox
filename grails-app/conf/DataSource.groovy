742e10a9883c97612e1ba36b1463ff8c486113bd
Muhammad Ichsan <ichsan@gmail.com>
2016-06-16 00:38:49 +0800

    Fix: profile items be gone on update profile

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -6,34 +6,28 @@ environments {
         grails {
             mongo {
 
-
-//                host = "localhost"
-//                port = 27017
-//                username = ""
-//                password = ""
-//                databaseName = "ticbox-wl"
+                /*
+                host = "localhost"
+                port = 27017
+                username = ""
+                password = ""
+                databaseName = "ticbox-wl"
+                */
                 //diaglog = 3
 
 
-                /*
                 host = "ds037215.mongolab.com"
                 port = 37215
                 username = "firman"
                 password = "redchair45"
                 databaseName = "ticboxdb-wl"
-                */
-
+/*
                 host = "ds045464.mongolab.com"
                 port = 45464
                 username = "firman"
                 password = "redchair45"
                 databaseName = "ticboxdb-demo"
-
-//                host = "ds025742.mlab.com"
-//                port = 25742
-//                username = "firman"
-//                password = "redchair45"
-//                databaseName = "ticbox-tbl"
+*/
             }
         }
     }
@@ -48,14 +42,6 @@ environments {
                 password = "redchair45"
                 databaseName = "ticboxdb-demo"
             }
-
-//            mongo {
-//                host = "ds025742.mlab.com"
-//                port = 25742
-//                username = "firman"
-//                password = "redchair45"
-//                databaseName = "ticbox-tbl"
-//            }
         }
     }
 }
